11d57f66b4c68cbd9f0035322c25770f976cf0b4
merk <leonardo.mercado193@tectijuana.edu.mx>
2022-07-29 11:25:06 -0700

    208

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -7,9 +7,20 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: AuthBackground(
-        child: SingleChildScrollView(child: Column(
+        child: SingleChildScrollView(
+          child: Column(
           children: [
-            
+            SizedBox(height: 250),
+            CardContainer(
+              child: Column(children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Text('Login', style: Theme.of(context).textTheme.headline4,),
+                SizedBox(height: 30,),
+                Text('Formulario'),
+              ]),
+            ),
           ],
         ),
         ),
