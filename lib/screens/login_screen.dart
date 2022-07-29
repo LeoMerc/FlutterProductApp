feece9ee2cb4fa7d8cc25c4f5a4b82413127f520
merk <leonardo.mercado193@tectijuana.edu.mx>
2022-07-29 11:49:02 -0700

    208

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,29 +2,74 @@ import 'package:flutter/material.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: AuthBackground(
         child: SingleChildScrollView(
           child: Column(
-          children: [
-            SizedBox(height: 250),
-            CardContainer(
-              child: Column(children: [
-                SizedBox(
-                  height: 10,
-                ),
-                Text('Login', style: Theme.of(context).textTheme.headline4,),
-                SizedBox(height: 30,),
-                Text('Formulario'),
-              ]),
-            ),
-          ],
-        ),
+            children: [
+              SizedBox(height: 250),
+              CardContainer(
+                child: Column(children: [
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Login',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  _LoginForm()
+                ]),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Crear una nueva cuenta',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+          //TODO mantener la referencia al KEY
+          child: Column(
+        children: [
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.deepPurple, width: 2),
+                ),
+                hintText: 'JohnDoe@gmail.com',
+                labelText: 'Correo',
+                labelStyle: TextStyle(
+                  color: Colors.grey,
+                ),
+                prefixIcon: Icon(Icons.alternate_email_sharp, color: Colors.deepPurple,)
+                ),
+          )
+        ],
+      )),
     );
   }
 }
