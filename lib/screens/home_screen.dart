1476904eb2c6347438407f8827f40912cf127c43
merk <leonardo.mercado193@tectijuana.edu.mx>
2022-08-10 13:16:02 -0700

    218

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
-
+import '../widgets/widgets.dart';
 class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
-      )
+      appBar: AppBar(
+        title: Text('Productos'),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index  ) => ProductCard()
+        ),
+        floatingActionButton: FloatingActionButton(child: Icon(Icons.add),
+        onPressed: (){},
+        ),
     );
   }
 }
---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ProductCard extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        margin: EdgeInsets.only(top: 30, bottom: 30),
+          width: double.infinity,
+          height: 400,
+          color: Colors.red,
+      ),
+    );
+  }
+}
