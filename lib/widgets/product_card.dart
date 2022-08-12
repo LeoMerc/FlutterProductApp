720a106c19fe641e200a33324f11c40f2f2e33a1
merk <leonardo.mercado193@tectijuana.edu.mx>
2022-08-12 11:10:45 -0700

    220

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -4,55 +4,160 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class ProductCard extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Container(
-        margin: EdgeInsets.only(top: 30, bottom: 30),
-          width: double.infinity,
-          height: 400,
-          decoration: _cardBorders(),
-          child: Stack(
-            alignment: Alignment.bottomLeft,
-            children: [
-              _BackgroundImage(),
-              _ProductDetails(),
-            ],
-          ),
+        margin: const EdgeInsets.only(top: 30, bottom: 30),
+        width: double.infinity,
+        height: 400,
+        decoration: _cardBorders(),
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            _BackgroundImage(),
+
+            _ProductDetails(),
+
+            Positioned(
+              top: 0,
+              right: 0,
+              child: _PriceTag(),
+            ),
+
+            Positioned(
+              top: 0,
+              left: 0,
+              child: _notAvailable(),
+            ),
+          ],
+        ),
       ),
     );
   }
 
   BoxDecoration _cardBorders() => BoxDecoration(
-    color: Colors.white,
-    borderRadius: BorderRadius.circular(25),
-    boxShadow: [BoxShadow(blurRadius: 10,
-    color: Colors.black,
-    offset: Offset(0,5),
-    )
-    ],
-
-  );
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: [
+          const BoxShadow(
+            blurRadius: 10,
+            color: Colors.black,
+            offset: Offset(0, 5),
+          )
+        ],
+      );
 }
 
-class _ProductDetails extends StatelessWidget {
-
-
+class _notAvailable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
+        width: 100,
+        height: 70,
+        decoration: BoxDecoration(
+            color: Colors.yellow[800],
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(25),
+              bottomRight: Radius.circular(25),
+            )),
+        child: const FittedBox(
+          fit: BoxFit.contain,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 10,
+            ),
+            child: Text(
+              'No disponible',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+        ));
+  }
+}
+
+class _PriceTag extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      width: 100,
       height: 70,
-      color: Colors.indigo,
+      decoration: const BoxDecoration(
+          color: Colors.indigo,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(25),
+            bottomLeft: Radius.circular(25),
+          )),
+      child: const FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 10,
+          ),
+          child: Text(
+            '\$103231.99',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
 
+class _ProductDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 50),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: double.infinity,
+        height: 70,
+        decoration: _BuildBoxDecoration(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'Disco Duro G',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              'Id Del Disco Duro',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _BuildBoxDecoration() => const BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(25),
+          topRight: Radius.circular(25),
+        ),
+      );
+}
+
 class _BackgroundImage extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
@@ -60,11 +165,11 @@ class _BackgroundImage extends StatelessWidget {
       child: Container(
         width: double.infinity,
         height: 400,
-        child: FadeInImage(
+        child: const FadeInImage(
           placeholder: AssetImage("assets/jar-loading.gif"),
           image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
           fit: BoxFit.cover,
-          ),
+        ),
       ),
     );
   }
