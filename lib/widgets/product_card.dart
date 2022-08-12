cb8f71c39dc3333cac7cdc90358030bbbf339d9a
merk <leonardo.mercado193@tectijuana.edu.mx>
2022-08-12 10:07:18 -0700

    219

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -13,7 +13,58 @@ class ProductCard extends StatelessWidget {
         margin: EdgeInsets.only(top: 30, bottom: 30),
           width: double.infinity,
           height: 400,
-          color: Colors.red,
+          decoration: _cardBorders(),
+          child: Stack(
+            alignment: Alignment.bottomLeft,
+            children: [
+              _BackgroundImage(),
+              _ProductDetails(),
+            ],
+          ),
+      ),
+    );
+  }
+
+  BoxDecoration _cardBorders() => BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.circular(25),
+    boxShadow: [BoxShadow(blurRadius: 10,
+    color: Colors.black,
+    offset: Offset(0,5),
+    )
+    ],
+
+  );
+}
+
+class _ProductDetails extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      color: Colors.indigo,
+    );
+  }
+}
+
+class _BackgroundImage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        width: double.infinity,
+        height: 400,
+        child: FadeInImage(
+          placeholder: AssetImage("assets/jar-loading.gif"),
+          image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
+          fit: BoxFit.cover,
+          ),
       ),
     );
   }
